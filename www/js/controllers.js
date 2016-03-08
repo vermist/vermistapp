7dd44f5eb6e89e46bd4d97490c42685a1d8c2115
regilioDielemans <regilio_dielemans@hotmail.com>
2016-03-08 10:05:09 +0100

    ionic

---
+++ www/js/controllers.js
@@ -0,0 +1,85 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+
+
+// .controller('FetchController', ['$scope', '$http', '$templateCache',
+//   function($scope, $http, $templateCache) {
+//     $scope.method = 'GET';
+//     $scope.url = 'http://rr-websites.nl/testmap/webapp/';
+
+//     $scope.fetch = function() {
+//       $scope.code = null;
+//       $scope.response = null;
+
+//       $http({method: $scope.method, url: $scope.url, cache: $templateCache}).
+//         then(function(response) {
+//           $scope.status = response.status;
+//           $scope.data = response.data;
+//         }, function(response) {
+//           $scope.data = response.data || "Request failed";
+//           $scope.status = response.status;
+//       });
+//     };
+
+//     $scope.updateModel = function(method, url) {
+//       $scope.method = method;
+//       $scope.url = url;
+//     };
+//   }]);
+
+
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+});
