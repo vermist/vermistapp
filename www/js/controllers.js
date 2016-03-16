771eebc87bba89492487e8f68d246b13561de790
regilioDielemans <regilio_dielemans@hotmail.com>
2016-03-16 15:01:08 +0100

    http post

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -86,6 +86,7 @@ var app_vermist = angular.module('app_vermist', ['ngRoute'])
   $scope.submitForm = function(valid){
     if (valid) {
       console.log('de form is gesubmit');
+      $http.post('http://rr-websites.nl/testmap/webapp/index.php?id=').then(function(response){
     }else{
       console.log('de form is niet valid');
     }
